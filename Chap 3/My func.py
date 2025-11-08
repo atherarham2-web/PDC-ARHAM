1628793a23c4fd1cb6d29a1d87cf03c022e6f402
atherarham2-web <atherarham2@gmail.com>
2025-11-08 16:36:56 +0500

    Create My func.py

---
+++ Chap 3/My func.py
@@ -0,0 +1,47 @@
+
+import math
+
+def perform_mensuration_calculations():
+    r = 7.5
+    l, b, h = 12, 8, 5
+    a = 6
+
+    area_circle = math.pi * r ** 2
+    peri_circle = 2 * math.pi * r
+    area_rect = l * b
+    peri_rect = 2 * (l + b)
+    area_square = a ** 2
+    peri_square = 4 * a
+    area_triangle = 0.5 * b * h
+
+    sa_cube = 6 * a ** 2
+    vol_cube = a ** 3
+    sa_cuboid = 2 * (l*b + b*h + h*l)
+    vol_cuboid = l * b * h
+    sa_cylinder = 2 * math.pi * r * (r + h)
+    vol_cylinder = math.pi * r ** 2 * h
+
+    total = (
+        area_circle + peri_circle + area_rect + peri_rect +
+        area_square + peri_square + area_triangle +
+        sa_cube + vol_cube + sa_cuboid + vol_cuboid +
+        sa_cylinder + vol_cylinder
+    )
+
+    for i in range(100):
+        total += math.sqrt(total * i + 1)
+
+    return total
+
+def myFunc(i):
+    print('calling myFunc from process n°: %s' % i)
+    
+    for j in range(0, i):
+        print('output from myFunc is : %s' % j)
+
+    result = perform_mensuration_calculations()
+    print("\nMensuration calculation result:", result)
+
+    return
+
+myFunc(5)
